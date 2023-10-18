61f83833648f1d2788deb83a5eaf140720c7dfe2
Avant Taylor <Avant.Taylor23@guttmanmail.cuny.edu>
2023-10-18 11:47:52 -0400

    User numbers

---
+++ User_numbers.py
@@ -0,0 +1,8 @@
+def Main()
+    print (this program adds two numbers.")
+    number1=  input (enter the first number:")
+    number2=  input (Enter the sceond number:)
+    total = int (number1)+ Int(number2)
+    print ("The total is " + total +".")
+if_name_=="_main_":
+    main()
