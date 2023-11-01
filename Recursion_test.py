be38b8ed44b6bb754a9d7783bb48ffee7ef81c74
Avant Taylor <145406811+Eminem0330@users.noreply.github.com>
2023-11-01 12:11:31 -0400

    commit

---
+++ Recursion_test.py
@@ -0,0 +1,16 @@
+def Stack_me(num1):
+    if num1==10:
+    Print(''add stack_me:'',num1)
+    print(''finished:'',num1)
+    return num1
+print(''add stack_me:''(num1)
+num1 +=1
+print(''remove_me'',num1)
+print(''return_me'',num1)
+def_main():
+    start =1
+    num1= stack _me(start)
+    print (''Back in Main'', end='' '')
+    print (''remove stack _me :'', num1)
+_name_==''_main_'':
+ main()
