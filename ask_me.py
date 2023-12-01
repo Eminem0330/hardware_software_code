2083dfc3460a7426bdda5147ff0d14e30b0689ad
Avant Taylor <145406811+Eminem0330@users.noreply.github.com>
2023-12-01 15:53:22 -0500

    20231201 office hours

--- ask_me.py
+++ ask_me.py
@@ -1,11 +1,20 @@
 def user_selection():
-    ans = input('Enter selction:')
+    print("1) Binary to Decimal")
+    print("2) Decimal to Binary")
+    print("Q) Quit")
+    print("q) Quit")
+    ans = input('Enter selction:').lower()
     if ans =='q':
         return False
     return True
 def main():
-      run_me = true
+      run_me = True
       while run_me:
             run_me= user_selection()
-if _name_ =="_main_":
+if __name__ =="__main__":
           main()
+    decimal to octal
+hexadecimal to binary
+decimal  to    estial
+decimal  to    hexadecimal
+binary   to    decimal
---
+++ conversion_calcutor.py
@@ -0,0 +1,11 @@
+def user_selection():
+    
+
+def main():
+    run_me = true
+    while run_me:
+          run_me= user_selection()
+
+
+if _name_ =="_main_":
+          main()
