b0670f8c7c0a7dc9307f94d24edeabcf556e70e4
Avant Taylor <145406811+Eminem0330@users.noreply.github.com>
2023-12-01 17:13:10 -0500

    20231201 office hours

--- ask_me.py
+++ ask_me.py
@@ -3,14 +3,48 @@ def user_selection():
     print("2) Decimal to Binary")
     print("Q) Quit")
     print("q) Quit")
-    ans = input('Enter selction:').lower()
+    ans = input('Enter selection:').lower()
     if ans =='q':
         return False
-    return True
+    return ans
+def check_selection(ans):
+    if ans == 'q':
+        return False
+    elif ans =="1":
+        return ans
+    elif ans =="2":
+        return ans
+    else:
+        print("Invalid Selection!!!")
+        return True
+
+def get_number(ans):
+    if ans == "1":
+        ans = input("Enter a binary number")
+    else:
+        ans = input("Enter a decimal number")
+
+    return ans
+
+def check_number(ans):
+    
+
+
+
+
+
+
+
+
+
 def main():
       run_me = True
       while run_me:
             run_me= user_selection()
+            run_me= check_selection(run_me)
+            run_me= get_number(run_me)
+            run_me= check_number(run_me)
+
 if __name__ =="__main__":
           main()
     decimal to octal
--- conversion_calcutor.py
+++ conversion_calcutor.py
@@ -1,11 +1,43 @@
 def user_selection():
-    
+    print("1) Binary to Decimal")
+    print("2) Decimal to Binary")
+    print("Q) Quit")
+    print("q) Quit")
+    ans = input('Enter selection:').lower()
+    if ans =='q':
+        return False
+    return ans
+def check_selection(ans):
+    if ans == 'q':
+        return False
+    elif ans =="1":
+        return ans
+    elif ans =="2":
+        return ans
+    else:
+        print("Invalid Selection!!!")
+        return True
+
+def get_number(ans):
+    if ans == "1":
+        ans = input("Enter a binary number")
+    else:
+        ans = input("Enter a decimal number")
+
+    return ans
+
+def check_number(ans):
+
+
+
 
 def main():
-    run_me = true
-    while run_me:
-          run_me= user_selection()
+      run_me = True
+      while run_me:
+            run_me= user_selection()
+            run_me= check_selection(run_me)
+            run_me= get_number(run_me)
+            run_me= check_number(run_me)
 
-
-if _name_ =="_main_":
+if __name__ =="__main__":
           main()
